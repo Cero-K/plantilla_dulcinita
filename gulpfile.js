b796a6a482c00a5b36fb34b1cfa35c57a7f467ad
@armandoaepp - Armando Enrique Pisfil Puemape <armanodaepp@gmail.com>
2017-05-02 13:16:21 -0500

    add files gulp

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp') ;
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+var cssnano = require('gulp-cssnano');
+
+
+
+
+// minificar todos los archivos JS
+gulp.task('min-js', function() {
+    return gulp.src([
+                      './assets/js/**/*.js',
+                    ])
+        .pipe(concat('app.js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+       .pipe(gulp.dest('./assets/js-min/'));
+});
+
+
+gulp.task('min-css', function () {
+    gulp.src([
+                './assets/css/**/*.css',
+              ])
+        .pipe(concat('app.css'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(cssnano())
+        .pipe(gulp.dest('./assets/css-min/'));
+});
+
