cde26c99b0dbf927087be06074cc7e5e42d44b15
Carlos Eduardo <crodriguez@lifedigital.pe>
2017-05-16 12:15:12 -0500

    Slider promociones avance2

--- assets/js/slider-home/slider-promociones.js
+++ assets/js/slider-home/slider-promociones.js
@@ -4,6 +4,10 @@ var slider2_promociones = $('#slider2-promociones');
 var slider3_promociones = $('#slider3-promociones');
 var slider4_promociones = $('#slider4-promociones');
 
+var slider5_promociones = $('#slider5-promociones');
+var slider6_promociones = $('#slider6-promociones');
+var slider7_promociones = $('#slider7-promociones');
+
 //almacenar botones
 var nextPromociones = $('#btn-next-promociones');
 var prevPromociones = $('#btn-prev-promociones');
@@ -14,11 +18,19 @@ $('#slider2-promociones .slider__section2-promociones:last').insertBefore('#slid
 $('#slider3-promociones .slider__section3-promociones:last').insertBefore('#slider3-promociones .slider__section3-promociones:first');
 $('#slider4-promociones .slider__section4-promociones:last').insertBefore('#slider4-promociones .slider__section4-promociones:first');
 
+$('#slider5-promociones .slider__section5-promociones:last').insertBefore('#slider5-promociones .slider__section5-promociones:first');
+$('#slider6-promociones .slider__section6-promociones:last').insertBefore('#slider6-promociones .slider__section6-promociones:first');
+$('#slider7-promociones .slider__section7-promociones:last').insertBefore('#slider7-promociones .slider__section7-promociones:first');
+
 //mostrar la primera imagen con un margen de -100%
 slider1_promociones.css('margin-left', '-'+100+'%');
 slider2_promociones.css('margin-left', '-'+100+'%');
 slider3_promociones.css('margin-left', '-'+100+'%');
 slider4_promociones.css('margin-left', '-'+100+'%');
+
+slider5_promociones.css('margin-left', '-'+100+'%');
+slider6_promociones.css('margin-left', '-'+100+'%');
+slider7_promociones.css('margin-left', '-'+100+'%');
 
 function moverDerecha_promociones() {
 	slider1_promociones.animate({
@@ -47,6 +59,24 @@ function moverDerecha_promociones() {
 	} ,1000, function(){
 		$('#slider4-promociones .slider__section4-promociones:first').insertAfter('#slider4-promociones .slider__section4-promociones:last');
 		slider4_promociones.css('margin-left', '-'+100+'%');
+	});
+	slider5_promociones.animate({
+		marginLeft:'-'+200+'%'
+	} ,1000, function(){
+		$('#slider5-promociones .slider__section5-promociones:first').insertAfter('#slider5-promociones .slider__section5-promociones:last');
+		slider5_promociones.css('margin-left', '-'+100+'%');
+	});
+	slider6_promociones.animate({
+		marginLeft:'-'+200+'%'
+	} ,1000, function(){
+		$('#slider6-promociones .slider__section6-promociones:first').insertAfter('#slider6-promociones .slider__section6-promociones:last');
+		slider6_promociones.css('margin-left', '-'+100+'%');
+	});
+	slider7_promociones.animate({
+		marginLeft:'-'+200+'%'
+	} ,1000, function(){
+		$('#slider7-promociones .slider__section7-promociones:first').insertAfter('#slider7-promociones .slider__section7-promociones:last');
+		slider7_promociones.css('margin-left', '-'+100+'%');
 	});
 }
 
@@ -77,6 +107,24 @@ function moverIzquierda_promociones() {
 	} ,700, function(){
 		$('#slider4-promociones .slider__section4-promociones:last').insertBefore('#slider4-promociones .slider__section4-promociones:first');
 		slider4_promociones.css('margin-left', '-'+100+'%');
+	});
+	slider5_promociones.animate({
+		marginLeft:0
+	} ,700, function(){
+		$('#slider5-promociones .slider__section5-promociones:last').insertBefore('#slider5-promociones .slider__section5-promociones:first');
+		slider5_promociones.css('margin-left', '-'+100+'%');
+	});
+	slider6_promociones.animate({
+		marginLeft:0
+	} ,700, function(){
+		$('#slider6-promociones .slider__section6-promociones:last').insertBefore('#slider6-promociones .slider__section6-promociones:first');
+		slider6_promociones.css('margin-left', '-'+100+'%');
+	});
+	slider7_promociones.animate({
+		marginLeft:0
+	} ,700, function(){
+		$('#slider7-promociones .slider__section7-promociones:last').insertBefore('#slider7-promociones .slider__section7-promociones:first');
+		slider7_promociones.css('margin-left', '-'+100+'%');
 	});
 }
 
